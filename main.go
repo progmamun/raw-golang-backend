1d10a1b4df4881c8c03c4d2caedd8c3a162d0617
progmamun <mamun333089@gmail.com>
2026-06-16 22:29:11 +0600

    get-single-and-updated

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
@@ -35,6 +36,8 @@ func main() {
 	mux.HandleFunc("GET /health", healthHandler)
 	mux.HandleFunc("POST /createUser", createUserHandler)
 	mux.HandleFunc("GET /users", getUserHandler)
+	mux.HandleFunc("GET /users/{id}", getSingleUserHandler)
+	mux.HandleFunc("PUT /users/{id}", updateSingleUserHandler)
 
 
 	fmt.Println("Server is running at port 5000")
@@ -76,4 +79,64 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 	// w.Write(users)
 	encoder := json.NewEncoder(w)
 	encoder.Encode(users)
+}
+
+func getSingleUserHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := r.PathValue("id")
+	// fmt.Printf("the value of id is %v and the type of the id is %T", idParam, idParam)
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Invalid user id")
+		return
+	}
+
+	for _, user := range users{
+		if user.Id == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "User not found")
+}
+
+func updateSingleUserHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := r.PathValue("id")
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Invalid user id")
+		return
+	}
+
+	var updatedUser User
+
+	err = json.NewDecoder(r.Body).Decode(&updatedUser)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid request body")
+		return
+	}
+
+	for idx, user := range users {
+		if user.Id == id {
+			updatedUser.Id = id
+			users[idx] = updatedUser
+
+			w.Header().Set("Content-type", "application/json")
+			json.NewEncoder(w).Encode(updatedUser)
+			return
+		}
+	}
+
+	
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "User not found")
 }
