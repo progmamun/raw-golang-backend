96b03c5c19a7e218dfd86b73bb7a58a7b1a38179
progmamun <mamun333089@gmail.com>
2026-06-16 21:55:58 +0600

    create-user-and-get

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type User struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Email string `json:"email"`
+}
+
+var users = []User{
+	{
+		Id: 1,
+		Name: "Mamun",
+		Age: 26,
+		Email: "mamun333089@gmail.com",
+	},
+	{
+		Id: 2,
+		Name: "Mamun2",
+		Age: 25,
+		Email: "testing2@gmail.com",
+	},
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", rootHandler)
-	mux.HandleFunc("/health", healthHandler)
+	mux.HandleFunc("GET /", rootHandler)
+	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc("POST /createUser", createUserHandler)
+	mux.HandleFunc("GET /users", getUserHandler)
 
 
 	fmt.Println("Server is running at port 5000")
@@ -26,4 +51,29 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Server is up and healthy")
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid request")
+		return
+	}
+	newUser.Id = len(users) + 1
+	users = append(users, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
+}
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// users, _ := json.Marshal(users)
+	// w.Write(users)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(users)
 }
